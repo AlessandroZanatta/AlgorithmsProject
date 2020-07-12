9ae85798d83441845c17b757be746c0c03fc25d6
AlesssandroZanatta <alessandro.zanatta.lav@gmail.com>
2020-07-12 10:33:32 -0400

    Implemented working HeapSelect algorithm. Will need to change for a better estimation of times

--- first_part/select_fun.c
+++ first_part/select_fun.c
@@ -4,6 +4,8 @@
 
 #include "select_fun.h"
 #include "utils.h"
+#include "../heap/minheap.h"
+#include "../heap/maxheap.h"
 
 int quickselect_rec(int *a, int p, int q, int k) {
     if(p == q) {
@@ -43,8 +45,54 @@ int quickselect(int * a, int length, int k){
  */
 int heapselect(int * a, int length, int k){
     if(k <= (int) (length / 2)){ // use min-heap (more-efficient)
+        struct MinHeap * H1 = buildMinHeap(a, length);
+        struct MinHeapPos * H2 = createEmptyMinHeapPos();
 
+        int res[2];
+
+        insertMinPos(H2, getMin(H1), 1);
+
+        for(int i = 1; i <= k; i++){
+            extractMinPos(H2, res);
+            int l = LEFT(res[POS]);
+            int r = RIGHT(res[POS]);
+
+            if(l <= heapsizeMin(H1)){
+                insertMinPos(H2, getKeyOnPositionMin(H1, l), l);
+            }
+
+            if(r <= heapsizeMin(H1)){
+                insertMinPos(H2, getKeyOnPositionMin(H1, r), r);
+            }
+        }
+
+
+        // destroyMinHeap(H1);
+        // destroyMinHeapPos(H2);
+
+        return getMinPos(H2);
     } else { // use max-heap (more efficient)
+        struct MaxHeap * H1 = buildMaxHeap(a, length);
+        struct MaxHeapPos * H2 = createEmptyMaxHeapPos();
+        k = length - k; // we need to perform length - k iterations if we use maxheaps!
+        int res[2];
 
+        insertMaxPos(H2, getMax(H1), 1);
+
+        for(int i = 1; i < k; i++){
+            extractMaxPos(H2, res);
+            int l = LEFT(res[POS]);
+            int r = RIGHT(res[POS]);
+
+            if(l <= heapsizeMax(H1)){
+                insertMaxPos(H2, getKeyOnPositionMax(H1, l), l);
+            }
+
+            if(r <= heapsizeMax(H1)){
+                insertMaxPos(H2, getKeyOnPositionMax(H1, r), r);
+            }
+        }
+
+        return getMaxPos(H2);
     }
 }
--- first_part/select_fun.h
+++ first_part/select_fun.h
@@ -6,5 +6,6 @@
 #define ALGORITHMPROJECT_SELECT_FUN_H
 
 int quickselect(int * a, int length, int k);
+int heapselect(int * a, int length, int k);
 
 #endif //ALGORITHMPROJECT_SELECT_FUN_H
